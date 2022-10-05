9a6df53382eb88d4ff745bf5a6a5455a26ee878f
nathan green <nathan@nathangreen.me>
2022-10-05 17:49:08 +0000

    store and load fareObjects as json

--- main.go
+++ main.go
@@ -5,50 +5,104 @@ package main
 import (
 	"archive/zip"
 	"io"
+	"time"
+
+	// "context"
+
 	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
 
+	"github.com/bojanz/currency"
 	"github.com/imnatgreen/busfares/internal/agency"
 	"github.com/imnatgreen/busfares/internal/fares"
+	"github.com/imnatgreen/busfares/internal/persist"
 	"github.com/imnatgreen/busfares/internal/router"
+	// "github.com/jackc/pgx"
 )
 
 func main() {
-	// fareDataset := fmt.Sprintf("https://data.bus-data.dft.gov.uk/api/v1/fares/dataset/?noc=BPTR,HRGT,KDTR,LNUD,ROST,TPEN,YACT,YCST&api_key=%s", os.Getenv("BODS_API_KEY"))
-	// res, err := http.Get(fareDataset)
+	// conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	// if err != nil {
-	// 	res.Body // https://stackoverflow.com/a/31129967
+	// 	log.Fatalf("unable to connect to database: %v", err)
 	// }
+	// defer conn.Close(context.Background())
+
+	var err error
+
+	var fareObjects fares.FareObjects
+
+	//downloadDatasets()
+	//addDatasets(&fareObjects)
+
+	// save fareObjects to disk
+	// start := time.Now()
+	// err = persist.Save("fareObjects.json", fareObjects)
+	// if err != nil {
+	// 	log.Fatalf("failed to save fareObjects: %v", err)
+	// }
+	// log.Printf("saved fares to disk in %s", time.Since(start))
+
+	// load fareObjects from disk
+	start := time.Now()
+	err = persist.Load("fareObjects.json", &fareObjects)
+	if err != nil {
+		log.Fatalf("failed to load fareObjects: %v", err)
+	}
+	log.Printf("loaded %d fareObjects from disk in %s", len(fareObjects.Objects), time.Since(start))
 
 	// load agencies from GTFS files
+	start = time.Now()
 	agencies, _ := loadAgencies(os.Getenv("GTFS_DIR"))
 	aTest, _ := agencies.GetNoc("OP291")
 	log.Print(aTest)
+	log.Printf("loaded %d agencies from disk in %s", len(agencies), time.Since(start))
 
 	// test router
 	json, _ := os.Open("internal/router/resp.json")
 	defer json.Close()
 	data, _ := io.ReadAll(json)
 	var res router.TripPlannerResponse
-	res, err := router.ParseJson(data)
+	res, err = router.ParseJson(data)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Print(res)
-	log.Print(res.Plan.Itineraries[1].Legs[1].To.Name)
+
+	start = time.Now()
+	err = res.AddFares(&fareObjects, &agencies)
+	log.Printf("added fares to response in %s", time.Since(start))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(currency.NewFormatter(currency.NewLocale("gb")).Format(res.Plan.Itineraries[1].Legs[1].Fares[16].Amount))
+	log.Print(res.Plan.Itineraries[1].Legs[1].Fares)
+	//log.Print(res)
+	//log.Print(res.Plan.Itineraries[1].Legs[1].To.Name)
 
 	// test fares
-	xml, _ := os.Open("internal/fares/ROST_483_Outbound_Single_.xml")
-	defer json.Close()
-	xmlData, _ := io.ReadAll(xml)
-	var xmlRes fares.FareObject
-	xmlRes, err = fares.ParseXml(xmlData)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Print(xmlRes)
+	// xml, _ := os.Open("internal/fares/ROST_483_Outbound_Single_.xml")
+	// defer xml.Close()
+	// xmlData, _ := io.ReadAll(xml)
+	// var fareData fares.FareObject
+
+	// start = time.Now()
+	// fareData, err = fares.ParseXml(xmlData)
+	// log.Printf("parsed fares in %s", time.Since(start))
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Print(fareData.Tariffs[0].ValidBetween.FromDate.Time)
+	// start = time.Now()
+	// fare, err := fareData.GetFare("25001425", "2500IMG2914")
+	// log.Printf("got fare in %s", time.Since(start))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Println(currency.NewFormatter(currency.NewLocale("gb")).Format(fare))
+	// log.Print(fareData)
 }
 
 func loadAgencies(gtfsDir string) (agencies agency.Agencies, err error) {
@@ -92,6 +146,25 @@ func loadAgencies(gtfsDir string) (agencies agency.Agencies, err error) {
 	}
 	log.Printf("imported %d agencies", len(agencies))
 	return agencies, nil
+}
+
+func downloadDatasets() error {
+	err := fares.GetDatasets("data/fares", os.Getenv("NOCS"))
+	if err != nil {
+		log.Fatalf("failed to get datasets: %v", err)
+		return err
+	}
+	return nil
+}
+
+func addDatasets(fareObjects *fares.FareObjects) error {
+	start := time.Now()
+	err := fareObjects.AddDir("data/fares")
+	if err != nil {
+		log.Fatalf("failed to add directory: %v", err)
+	}
+	log.Printf("parsed all fares in %s", time.Since(start))
+	return nil
 }
 
 // https://github.com/antchfx/xmlquery ? -> xpath?
